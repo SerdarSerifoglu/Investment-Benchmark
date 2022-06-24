bfc10355a28b17fc2016042b79739cd812753da9
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-06-24 08:17:31 +0300

    Emotion library's simple sample.

--- frontend/src/components/NavbarTop.js
+++ frontend/src/components/NavbarTop.js
@@ -1,13 +1,29 @@
 import { NavLink } from "react-router-dom";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
 const NavbarTop = () => {
   return (
     <>
       <div className="navbar">
-        <NavLink to="/main-page">Ana Sayfa</NavLink> | 
-        <NavLink to="/cumulative-report">Birikimli Yatırım</NavLink>
+        <NavLinkStyled to="/main-page">Ana Sayfa</NavLinkStyled> | 
+        <NavLinkStyled to="/cumulative-report">Birikimli Yatırım</NavLinkStyled>
       </div>
     </>
   );
 };
+const NavLinkStyled = styled(NavLink)`
+  color: black;
+  text-decoration: none;
 
+  &:hover {
+    font-weight: bold;
+    color: coral;
+  }
+
+  &.active {
+    font-weight: bold;
+    color: orange;
+  }
+`;
 export default NavbarTop;

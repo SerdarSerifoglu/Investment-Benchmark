8e6ca760ca967dcbeb8e0fd154b0aaf5d74da9f0
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-06-26 08:32:33 +0300

    change build script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require("cors");
 const path = require("path");
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "")));
 
 const port = 3000;
 

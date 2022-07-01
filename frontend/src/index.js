8b85c1d943f3836c2692dc8f3bf2218074d83f51
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-07-01 08:31:45 +0300

    add chart.js and react-chartjs-2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,60 @@ import App from "./App";
 import { render } from "react-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle,
+} from "chart.js";
 
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
 render(
   <React.StrictMode>
     <Provider store={store}>

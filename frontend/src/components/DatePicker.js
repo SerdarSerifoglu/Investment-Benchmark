88c2993a495b0d0a946bc1cd9507bf7f9d2e6a1e
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-07-01 11:52:42 +0300

    date picker min-max fixed temporary

--- frontend/src/components/DatePicker.js
+++ frontend/src/components/DatePicker.js
@@ -10,7 +10,16 @@ export const DatePicker = ({ fieldProperty, valueProperty, stateName }) => {
       id={fieldProperty}
       name={fieldProperty}
       defaultValue={valueProperty}
+      min="2000-01-01"
+      max="2022-06-28"
       onChange={(e) => {
+        //Max Min seçimi düzenlenecek sonradan
+        if (parseInt(e.target.value.replaceAll("-", "")) > 20220628) {
+          e.target.value = "2022-06-28";
+        }
+        if (parseInt(e.target.value.replaceAll("-", "")) < 20000101) {
+          e.target.value = "2000-01-01";
+        }
         dispatch(
           changeReportFilter({
             fieldProperty,

4e9fb54ef95b4148d197804f81d21b008f311399
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-07-01 12:00:56 +0300

    publish

--- frontend/src/http-client/httpClientService.js
+++ frontend/src/http-client/httpClientService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const httpClientService = axios.create();
 
-httpClientService.defaults.baseURL = "http://localhost:3000/api/";
-// httpClientService.defaults.baseURL = "https://investhero-v2.herokuapp.com/api/";
+// httpClientService.defaults.baseURL = "http://localhost:3000/api/";
+httpClientService.defaults.baseURL = "https://investhero-v2.herokuapp.com/api/";
 
 // axiosService.defaults.headers.common[
 //     "Authorization"

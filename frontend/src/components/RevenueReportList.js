5a75ab3e9aba53528b916715d868a710f3f1874f
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-07-08 13:37:08 +0300

    clean code

--- frontend/src/components/RevenueReportList.js
+++ frontend/src/components/RevenueReportList.js
@@ -34,7 +34,6 @@ const RevenueReportList = () => {
           {reportData.map((row) => (
             <TableRow
               key={row.investTypeValue}
-              placeholder="serdar"
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">

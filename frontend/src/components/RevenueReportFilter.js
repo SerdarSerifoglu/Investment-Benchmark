3433884b94d48ab27748cf52ce52260109bf8ef3
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-07-08 14:05:32 +0300

    MuiAutocomplete css manipulated

--- frontend/src/components/RevenueReportFilter.js
+++ frontend/src/components/RevenueReportFilter.js
@@ -1,5 +1,5 @@
 import AutocompleteMultiSelect from "./AutocompleteMultiSelect";
-import { Button, TextField } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { DatePicker } from "./DatePicker";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -19,39 +19,40 @@ const RevenueReportFilter = () => {
 
   return (
     <>
-      <div>
-        <AutocompleteMultiSelect
-          fieldProperty="investmentType"
-          valueProperty={reportFilterData["investmentType"]}
-          stateName={componentNames.RevenueReportFilter}
-        />
-      </div>
-      <br />
-      <div>
-        <DatePicker
-          fieldProperty="startDate"
-          valueProperty={reportFilterData["startDate"]}
-          stateName={componentNames.RevenueReportFilter}
-        />
-        <DatePicker
-          fieldProperty="endDate"
-          valueProperty={reportFilterData["endDate"]}
-          stateName={componentNames.RevenueReportFilter}
-        />
-      </div>
-      <br />
-      <div>
-        <TextFieldNumber
-          labelName="Yatırım Miktarı"
-          fieldProperty="investmentAmount"
-          valueProperty={reportFilterData["investmentAmount"]}
-          stateName={componentNames.RevenueReportFilter}
-        />
-      </div>
-      <br />
-      <Button variant="contained" onClick={buttonClickEvent}>
-        Hesapla
-      </Button>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <AutocompleteMultiSelect
+            fieldProperty="investmentType"
+            valueProperty={reportFilterData["investmentType"]}
+            stateName={componentNames.RevenueReportFilter}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <DatePicker
+            fieldProperty="startDate"
+            valueProperty={reportFilterData["startDate"]}
+            stateName={componentNames.RevenueReportFilter}
+          />
+          <DatePicker
+            fieldProperty="endDate"
+            valueProperty={reportFilterData["endDate"]}
+            stateName={componentNames.RevenueReportFilter}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <TextFieldNumber
+            labelName="Yatırım Miktarı"
+            fieldProperty="investmentAmount"
+            valueProperty={reportFilterData["investmentAmount"]}
+            stateName={componentNames.RevenueReportFilter}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <Button variant="contained" onClick={buttonClickEvent}>
+            Hesapla
+          </Button>
+        </Grid>
+      </Grid>
     </>
   );
 };
